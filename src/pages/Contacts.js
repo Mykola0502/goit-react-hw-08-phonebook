42181e6494b85082ef6d95b1307d0358cbb2a06b
Mykola Saliuk <haker0502@ukr.net>
2023-03-02 23:55:24 +0200

    додає маршрутизацію

---
+++ src/pages/Contacts.js
@@ -0,0 +1,5 @@
+const Contacts = () => {
+  return <h1>ContactsPage</h1>;
+};
+
+export default Contacts;
---
+++ src/pages/Register.js
@@ -0,0 +1,5 @@
+const Register = () => {
+  return <h1>RegisterPage</h1>;
+};
+
+export default Register;
